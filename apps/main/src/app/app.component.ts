3e700774278132f752ede308d1853a45a1fe5154
fleish80 <fleish.d@gmail.com>
2023-11-30 14:43:07 +0200

    logger

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import {LoggerService} from "@esquare-mission/logger";
 
 @Component({
   standalone: true,
@@ -17,20 +16,5 @@ import {LoggerService} from "@esquare-mission/logger";
   `
 })
 export class AppComponent {
-
-  #loggerService = inject(LoggerService);
-
-  constructor() {
-    try {
-
-      let arr: number[] | null = [];
-      arr[10] = 5;
-      arr = null;
-      arr![10] = 5;
-    } catch (error) {
-      this.#loggerService.log(error as any)
-    }
-  }
-
 
 }
--- apps/main/src/app/app.config.ts
+++ apps/main/src/app/app.config.ts
@@ -3,7 +3,7 @@ import {provideRouter} from '@angular/router';
 import {appRoutes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {environment} from "../environments/environment";
-import {provideLogger} from "@esquare-mission/logger";
+import {provideLogger} from "@logger-mission/logger";
 import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
